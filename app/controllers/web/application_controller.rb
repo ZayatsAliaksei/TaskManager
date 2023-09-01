4a250c5b7078ccc5a8f845d43d123d66b9ab6e15
ZayatsAliaksei <Lehazayac@tut.by>
2023-09-01 18:18:24 +0400

    fix bugs

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class Web::ApplicationController < ApplicationController
-  include Concerns::AuthHelper
+  include AuthHelper  # Change this line
   helper_method :current_user
 end
